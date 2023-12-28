c889eabdb373df901ece22ebbe7ebc5aac0e2929
root718 <154349675+root718@users.noreply.github.com>
2023-12-28 19:14:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,10 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.me.username
+    app2 = web.AppRunner(await web_server())
+    await app2.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app2, bind_address, PORT).start()
     logging.info(f"@{uname} is now running!")
     idle()
     app.stop()
