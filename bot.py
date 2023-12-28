7266f87ccae779fcbb52dfbbe7f956934cf78874
root718 <154349675+root718@users.noreply.github.com>
2023-12-28 19:03:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 from aiohttp import web
+from plugins import web_server
 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
@@ -27,10 +28,6 @@ if __name__ == "__main__":
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.me.username
     logging.info(f"@{uname} is now running!")
-    app2 = web.AppRunner(web_server())
-    app2.setup()
-    bind_address = "0.0.0.0"
-    web.TCPSite(app2, bind_address, PORT).start()
     idle()
     app.stop()
     logging.info("Bot stopped. Alvida!")
